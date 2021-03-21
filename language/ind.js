6ca89c7b02045e5468468adcb87529ceefc332df
xzinn77 <xzinnff0@gamil.com>
2021-03-21 14:26:41 -0400

    First commit

---
+++ language/ind.js
@@ -0,0 +1,305 @@
+exports.wait = () => {
+	return`*Análise 🤔*`
+}
+
+exports.succes = () => {
+	return`*Feito*`
+}
+
+exports.lvlon = () => {
+	return`*「 HABILITAR 」 LEVELING*`
+}
+
+exports.lvloff = () => {
+	return`*「 DESATIVAR 」 LEVELING*`
+}
+
+exports.lvlnul = () => {
+	return`*SEU NÍVEL AINDA ESTÁ VAZIO*`
+}
+
+exports.lvlnoon = () => {
+	return`*O NÍVEL DO GRUB NÃO FOI ATIVADO*`
+}
+
+exports.noregis = () => {
+	return`*「 SEU LOGIN 」*\n\n*como logar ${prefix}daftar nome|idade* \n*exemplo ${prefix}daftar Lyno|27*`
+}
+
+exports.rediregis = () => {
+	return`*「 JÁ REGISTRADO 」*\n\n*você já está registrado no banco de dados do bot*`
+}
+
+exports.stikga = () => {
+	return`*sim falhou tente repetir mais alguns momentos*`
+}
+
+exports.linkga = () => {
+	return`*desculpe link inválido*`
+}
+
+exports.groupo = () => {
+	return`*「apenas p grupo」*`
+}
+
+exports.ownerb = () => {
+	return`*「so o dono do bot pode usar」*`
+}
+
+exports.ownerg = () => {
+	return`*「somente o dono do grupo」*`
+}
+
+exports.admin = () => {
+	return`*「So pros adm do grupo」*`
+}
+
+exports.badmin = () => {
+	return`*「O BOT DEVE SER ADMINISTRADOR」*`
+}
+
+exports.nsfwoff = () => {
+	return`*NSFW EM ATIVO*`
+}
+
+exports.bug = () => {
+	return`*O problema foi relatado ao proprietário do BOT, um relatório falso/main2 não será respondido*`
+}
+
+exports.wrongf = () => {
+	return`*formato incorreto*`
+}
+
+exports.clears = () => {
+	return`*clear all Success*`
+}
+
+exports.pc = () => {
+	return`*「 CADASTRO 」*\n\nPara saber se você se cadastrou, verifique a mensagem que enviei \n\nNOTE:\n*se você não entendeu a mensagem. significa que você não salvou o número do seu bot*`
+}
+
+exports.registered = (namaUser, umurUser, serialUser, time, sender) => {
+	return`*「 SEU PERFIL 」*\n\nvocê se registrou com os dados \n\n┏━⊱Nick\n┗⊱${namaUser}\n┏━⊱Numero\n┗⊱wa.me/${sender.split("@")[0]}\n┏━⊱idade\n┗⊱${umurUser}\n┏━⊱Hora q vc se registrou\n┗⊱${time}\n\n┏━❉ *NS* ❉━\n┣⊱${serialUser}\n┗⊱NOTE : não se esqueça deste número porque é importante:v`
+}
+
+exports.cmdnf = (prefix, command) => {
+	return`command *${prefix}${command}* não encontrado\tente escrever *${prefix}menu*`
+}
+
+exports.owneresce = (pushname) => {
+	return`*Desculpe, mas ${pushname} não o script do dono*`
+}
+
+exports.reglevelaha = (command, pushname, getLevelingLevel, sender, aha) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${aha}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahb = (command, pushname, getLevelingLevel, sender, ahb) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahb}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahc = (command, pushname, getLevelingLevel, sender, ahc) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahc}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahd = (command, pushname, getLevelingLevel, sender, ahd) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱nível : ${getLevelingLevel(sender)}*\n*┣⊱modelo command : ${command}*\n*┗⊱syarat level : ${ahd}*\n\n_NOTE : CHAT / SEMPRE NÃO PARA OBTER XP_`
+}
+
+exports.reglevelahe = (command, pushname, getLevelingLevel, sender, ahe) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahe}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahf = (command, pushname, getLevelingLevel, sender, ahf) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahf}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp, _registered, uangku, role, premi) => { 
+	return `
+║╭─┅┈ै✾⃗┈❫❫🔥⌝ː◈ː⌜🔥❪❪┈⃖✾ै┈┅─╮
+║
+║╭─━─━─━─━─━─━─━─╮
+║│➣ 𝐓𝐑𝐈𝐏𝐏𝐈𝚵 𝐁𝐎𝐓ϟ
+║│➣ 𝐏𝐑𝐄𝐅𝐈𝐗: ( . )
+║│➣ *wa.me/558897187831*
+║│➣ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║╰─━─━─━─━─━─━─━─╯
+║
+║╭─━─━─━─━─━─━─━─━─━─╮
+║│➣ *nome* : ${pushname}
+║│
+║│➣ *ua mi 😁* : wa.me/${sender.split("@")[0]}
+║│
+║│➣ *XP* : ${getLevelingXp(sender)}/${reqXp}
+║│
+║│➣ *level* : ${getLevelingLevel(sender)}
+║│
+║│➣ *user register : ${_registered.length}
+║╰─━─━─━─━─━─━─━─━─━─╯
+║
+╠━━━━━━━━━❮MENU❯━━━━━━━━━
+║
+║╭─━─━─━─━─━─╮
+║│➣ *${prefix}sticker*    
+║│➣ *${prefix}tts*
+║│➣ *${prefix}toimg*  
+║╰─━─━─━─━─━─╯
+║
+╠━━━━━━━━━━━━━━━━
+║
+║╭─━─━─━─━─━─━─╮
+║│➣ *${prefix}ocr*
+║│➣ *${prefix}quotemaker*
+║│➣ *${prefix}tiktokstalk*
+║│➣ *${prefix}fototiktok*
+║│➣ *${prefix}truth*
+║│➣ *${prefix}dare*
+║╰─━─━─━─━─━─━─╯
+║
+╠━━━━━━━━━━━━━━━━
+║
+║╭─━─━─━─━─━─╮
+║│➣ *${prefix}text3d*
+║│➣ *${prefix}bucin*
+║│➣ *${prefix}hilih*
+║│➣ *${prefix}ssweb*
+║╰─━─━─━─━─━─╯
+╠━━━━━━━━━━━━━━━━
+║
+║╭─━─━─━─━─━─━─╮
+║│➣ *${prefix}artinama*
+║│➣ *${prefix}spamcall*
+║│➣ *${prefix}quotes*
+║│➣ *${prefix}kbbi*
+║│➣ *${prefix}map*
+║│➣ *${prefix}cerpen*
+║╰─━─━─━─━─━─━─╯
+║
+╠━━━━━━━━━━━━━━━━
+║
+║╭─━─━─━─━─━─╮
+║│➣ *${prefix}chord*
+║│➣ *${prefix}lirik*
+║│➣ *${prefix}phlogo*
+║╰─━─━─━─━─━─╯
+║
+╠━━━━━━━━━━━━━━━━
+║
+║╭─━─━─━─━─━─━─╮
+║│➣ *${prefix}bisakah*
+║│➣ *${prefix}apakah*
+║│➣ *${prefix}kapankah*
+║│➣ *${prefix}rate*
+║│➣ *${prefix}owner*
+║╰─━─━─━─━─━─━─╯
+║
+╠━━━━━━━━❮NSFW❯━━━━━━━━
+║
+║╭─━─━─━─━─━─━─╮
+║│➣ *${prefix}waifu*
+║│➣ *${prefix}anime*
+║│➣ *${prefix}neko*
+║│➣ *${prefix}dewabatch*
+║│➣ *${prefix}pokemon*
+║│➣ *${prefix}anjing*
+║│➣ *${prefix}indohot*
+║╰─━─━─━─━─━─━─╯
+║
+╠━━━━━━━━❮GRUPO❯━━━━━━━━
+║
+║╭─━─━─━─━─━─━─━─━─━─━─╮
+║│➣ *${prefix}hidetag*
+║│➣ *${prefix}linkgc*
+║│➣ *${prefix}tagall*
+║│➣ *${prefix}setpp*
+║│➣ *${prefix}antilink
+║│➣ *${prefix}add*
+║│➣ *${prefix}kick*
+║│➣ *${prefix}setname*
+║│➣ *${prefix}setdesc*
+║│➣ *${prefix}demote*
+║│➣ *${prefix}kickall*
+║│➣ *${prefix}promote*
+║│➣ *${prefix}listadmin*
+║│➣ *${prefix}group* [buka/tutup]
+║│➣ *${prefix}leveling* [enable/disable]
+║│➣ *${prefix}nsfw* [1/0]
+║│➣ *${prefix}simih* [1/0]
+║│➣ *${prefix}welcome* [1/0]
+║╰─━─━─━─━─━─━─━─━─━─━─╯
+║
+╠━━━━━━━━❮OWNER❯━━━━━━━━
+║
+║╭─━─━─━─━─━─━─╮
+║│➣ *${prefix}bc*
+║│➣ *${prefix}setprefix*
+║│➣ *${prefix}getses*
+║│➣ *${prefix}clearall*
+║│➣ *${prefix}block*
+║│➣ *${prefix}unblock* 
+║│➣ *${prefix}leave*    
+║│➣ *${prefix}clone*
+║╰─━─━─━─━─━─━─╯
+║
+┗━━━━━━━━━━━━━━━━
+`
+}
+
+exports.levelup = (pushname, sender, getLevelingXp,  getLevel, getLevelingLevel, role) => {
+	return`
+╠━━━━━━━━❮Parabéns❯━━━━━━━━
+║╭─━─━─━─━─━─━─━─━─━─━─╮
+║│➣ *nome* : ${pushname}
+║│
+║│➣ *ua mi 😁* : wa.me/${sender.split("@")[0]}
+║│
+║│➣ *XP* : ${getLevelingXp(sender)}/${reqXp}
+║│
+║│➣ *level* : ${getLevelingLevel(sender)}
+║│
+║│➣*Limit* : +3
+║│
+║│➣ *Role*: ${role}
+║│
+║│➣ *user register : ${_registered.length}
+║╰─━─━─━─━─━─━─━─━─━─━─╯
+`}
+ 
+exports.limitend = (pushname) => {
+	return`*Desculpe ${pushname} O limite de hoje expira*\n*compre limite para obter limite/ Upar*`
+}
+
+exports.limitcount = (limitCounts) => {
+	return`
+*「 LIMIT COUNT 」*
+o resto do seu limite : ${limitCounts}
+
+NOTE : para chegar ao limite. pode passar de nível ou limite de capacidade`
+}
+
+exports.satukos = () => {
+	return`*Adicionar parâmetros 1/habilitar ou 0/desabilitar`
+}
+
+exports.uangkau = (pushname, sender, uangkau) => {
+	return`*┏⊱ 「 ATM 」⊰━┓*\n┣⊱ *nome* : ${pushname}\n┣⊱ *número* : ${sender.split("@")[0]}\n┣⊱ *Dinheiro* : ${uangkau}\n┗━━━━━━━━━━`
+}
+
+exports.premadd = (pnom) => {
+	return`*「 PREMIUM ADD 」*
+
+*Name* : ${pnom}
+*Expired* : 30 DAY\n*obrigado por pedido premium*`
+}
+
+exports.dellprem = (hnom) => {
+	return`*「 PREMIUM DELETE 」*
+
+*Name* : ${hnom}
+*Expired* : NOW:v\n*obrigado pelo pedido premium de volta em breve para comprar novamente:D*`
+}
+
+exports.premon = (pushname) => {
+	return`Desculpe ${pushname} VOCÊ NÃO É UM USUÁRIO PREMIUM`
+}
+
